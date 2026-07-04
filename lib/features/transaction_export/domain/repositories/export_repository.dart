e6d6e8dfa07947675d5f6be3dbf2b1abfc7fe572
Zakiur22 <zakiur22@gmail.com>
2026-07-04 09:15:54 +0000

    feat(bethriftytoday): add ExportRepository interface in transaction_export feature

---
+++ lib/features/transaction_export/domain/repositories/export_repository.dart
@@ -0,0 +1,10 @@
+import 'package:bethriftytoday/models/models.dart';
+import '../models/export_format.dart';
+
+abstract class ExportRepository {
+  Future<void> exportTransactions({
+    required List<Transaction> transactions,
+    required ExportFormat format,
+    required String currencySymbol,
+  });
+}
